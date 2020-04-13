65639f61d098534876ae814c3738297b6f232675
Hugo Avila <hugoaf@yahoo.com.mx>
2020-04-12 20:52:24 -0700

    My Solution
    https://imminent-axiomatic-cardinal.glitch.me/

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 const app         = express();
-
+const bcrypt      = require('bcrypt');
 fccTesting(app); //For FCC testing purposes
 
 const saltRounds = 12;
@@ -11,16 +11,22 @@ const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
 
 
+
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-
-
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash)=>{
+  console.log(hash);
+  bcrypt.compare(myPlaintextPassword,hash,(err, res)=>{
+    console.log(res);
+  })
+})
 
 //END_ASYNC
 
 //START_SYNC
 
-
-
+const hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+const result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log("result", result)
 //END_SYNC
 
 
